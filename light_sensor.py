14fda4e37d09daafa448100a16dbd324ed868d30
Yang <2295366402@qq.com>
2017-08-30 21:10:32 +0800

    upload_data

--- light_sensor.py
+++ light_sensor.py
@@ -7,8 +7,12 @@ def get_Light():
     bus=smbus.SMBus(1)
     addr=0x23
     data=bus.read_i2c_block_data(addr,0x11)
-    l = str((data[1] + (256 * data[0])) / 1.2)
+    l = str(round((data[1] + (256 * data[0])) / 1.2,1))
     arr.append(l)
+    if arr:
+        pass      
+    else:
+        print 'not light'
+        arr = get_Light()     
     return arr
-    GPIO.cleanup() 
-
+    GPIO.cleanup()
